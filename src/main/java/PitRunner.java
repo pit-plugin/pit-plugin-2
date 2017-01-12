6b085eaf74b7372a42e776d7e93d8d3a3a0e5c54
bogdan <pawel.bogdan@uj.edu.pl>
2017-01-12 03:35:11 +0100

    Calling mutation tests.
    "U mnie działa"

---
+++ src/main/java/PitRunner.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by pawel on 12.01.17.
+ */
+public class PitRunner {
+    public void runMutation() throws IOException, InterruptedException {
+
+        String command = String.format("java  " +
+                        "-cp %s  " +
+                        "org.pitest.mutationtest.commandline.MutationCoverageReport  " +
+                        "--classPath %s " +
+                        "--reportDir %s " +
+                        "--targetClasses %s " +
+                        "--targetTests %s " +
+                        "--sourceDirs %s",
+                "./lib/pitest-1.1.11.jar:./lib/pitest-command-line-1.1.11.jar:./lib/junit-4.12.jar:./lib/hamcrest-core-1.3.jar",
+                "/home/pawel/libs/pit-example-master/target/classes,/home/pawel/libs/pit-example-master/target/test-classes",
+                "/home/pawel/temp",
+                "pitexample.*",
+                "pitexample.*",
+                "/home/pawel/libs/pit-example-master"
+        );
+
+        System.out.println(command);
+        Process process = Runtime.getRuntime().exec(command);
+
+
+        Scanner sc = new Scanner(process.getInputStream());
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+        sc = new Scanner(process.getErrorStream());
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+        process.waitFor();
+
+    }
+}
--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,10 +1,12 @@
-import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
 
 public class SimpleTest {
 
     @Test
-    public void test() {
-        Assert.assertTrue(true);
+    public void test() throws IOException, InterruptedException {
+        PitRunner runner = new PitRunner();
+        runner.runMutation();
     }
 }
