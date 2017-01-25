661b94f58d8a181ae36e28bc5d49bd310f87b196
alcatras <klimalakamil@gmail.com>
2017-01-25 11:16:33 +0100

    Added few comments to code

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -7,6 +7,10 @@ import com.intellij.openapi.project.Project;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Class used to print output to PitPlugin console.
+ * note: Need to be dispatched when closing project.
+ */
 public class Output {
     private static final Object lock = new Object();
     private static Output instance;
@@ -15,6 +19,9 @@ public class Output {
     private Output() {
     }
 
+    /**
+     * Gets an instance of Output class.
+     */
     public static Output getInstance() {
         if (instance == null) {
             synchronized (lock) {
@@ -43,24 +50,35 @@ public class Output {
         return consoleView;
     }
 
+    /**
+     * Prints text followed by line feed to PitPlugin console.
+     */
     public void println(String text) {
         ConsoleView cv = getConsoleView();
         cv.print(text, ConsoleViewContentType.NORMAL_OUTPUT);
         cv.print("\n", ConsoleViewContentType.NORMAL_OUTPUT);
     }
 
+    /**
+     * Prints error followed by line feed to PitPlugin console.
+     */
     public void error(String error) {
         ConsoleView cv = getConsoleView();
         cv.print(error, ConsoleViewContentType.ERROR_OUTPUT);
         cv.print("\n", ConsoleViewContentType.ERROR_OUTPUT);
     }
 
+    /**
+     * Prints hyperlink to PitPlugin console.
+     */
     public void hyperlink(String hyperlink, HyperlinkInfo info) {
         ConsoleView cv = getConsoleView();
         cv.printHyperlink(hyperlink, info);
-        cv.print("\n", ConsoleViewContentType.NORMAL_OUTPUT);
     }
 
+    /**
+     * Disposed the console resources, need to be called when closing a project.
+     */
     public void dispose() {
         if (consoleView != null)
             consoleView.dispose();
