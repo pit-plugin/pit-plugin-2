226a1737833b1a2ed6f69f0bc63a0c2b93a3fb0b
BartK <bkrol950@gmail.com>
2017-02-01 17:58:39 +0100

    Pliki konfiguracyjne

--- src/main/java/TestAction.java
+++ src/main/java/TestAction.java
@@ -3,11 +3,11 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 
 public class TestAction extends AnAction {
     public TestAction() {
-        super("Test");
+        super("Pit Plugin");
     }
 
     public void actionPerformed(AnActionEvent event) {
-        Output.getInstance().println("sample output");
-        Output.getInstance().error("sample error");
+        Output.getInstance().println("Pit plugin");
+        Output.getInstance().error("Pit plugin error");
     }
 }
